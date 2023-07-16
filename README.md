d3f5e0728fbaa85cfee4ef11e5fa74d2ef42761f
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-07-16 22:16:34 +1000

    tests created for most methods - still to look at place and run

---
+++ README.md
@@ -0,0 +1,17 @@
+- ensure you have ruby installed (ruby -v)
+- copy over everything (git clone ...)
+- open folder (cd toy-robot)
+- bundle init
+- bundle install
+
+- testing!
+
+- run using ruby lib/toy-robot.rb
+  - give another name to this later
+
+- inputs
+  - PLACE X,Y,F
+  - MOVE
+  - LEFT
+  - RIGHT
+  - REPORT
--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,14 +1,14 @@
 require 'pry-byebug'
 
 class ToyRobot
-  attr_accessor :x, :y, :direction
+  attr_accessor :x, :y, :direction, :robot_on_table
 
-  def initialize
-    @table_length = 5
-    @x = 0
-    @y = 0
-    @direction = 'NORTH'
-    @robot_on_table = false
+  def initialize(x: 0, y: 0, direction: 'NORTH', on_table: false, table_length: 5)
+    @table_length = table_length
+    @x = x
+    @y = y
+    @direction = direction
+    @robot_on_table = on_table
   end
 
   def place(input)
@@ -73,42 +73,44 @@ class ToyRobot
     "Current position on the tabletop: #{@x}, #{@y}, #{@direction}"
   end
 
+  def self.run
+    robot = ToyRobot.new
+
+    puts '--- TOY ROBOT GAME ---'
+    puts 'Input your first command (first command must be PLACE X, Y, DIRECTION)'
+
+    # ENSURE PLACE COMMAND IS VALID
+    input = gets.chomp.upcase
+    until input.start_with?('PLACE')
+      puts 'First command must be PLACE'
+      input = gets.chomp.upcase
+    end
+
+    loop do
+      case input
+      when /\APLACE/
+        robot.place(input)
+      when 'MOVE'
+        robot.move
+      when 'LEFT'
+        robot.left
+      when 'RIGHT'
+        robot.right
+      when 'REPORT'
+        puts robot.report
+      when 'END'
+        puts 'Goodbye'
+        break
+      else
+        puts 'Wrong input, try again.'
+      end
+      input = gets.chomp.upcase
+    end
+  end
+
   private
 
   def out_of_scope?(position)
     position.negative? || position > @table_length - 1
-  end
-
-  robot = ToyRobot.new
-
-  puts '--- TOY ROBOT GAME ---'
-  puts 'Input your first command (first command must be PLACE X, Y, DIRECTION)'
-
-  # ENSURE PLACE COMMAND IS VALID
-  input = gets.chomp.upcase
-  until input.start_with?('PLACE')
-    puts 'First command must be PLACE'
-    input = gets.chomp.upcase
-  end
-
-  loop do
-    case input
-    when /\APLACE/
-      robot.place(input)
-    when 'MOVE'
-      robot.move
-    when 'LEFT'
-      robot.left
-    when 'RIGHT'
-      robot.right
-    when 'REPORT'
-      puts robot.report
-    when 'END'
-      puts 'Goodbye'
-      break
-    else
-      puts 'Wrong input, try again.'
-    end
-    input = gets.chomp.upcase
   end
 end
