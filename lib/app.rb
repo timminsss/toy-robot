a05e575f170fb52c269dd45594cc06234c1d46e9
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-07-17 20:08:39 +1000

    refactored app.rb to move various messages to methods

--- lib/app.rb
+++ lib/app.rb
@@ -3,15 +3,32 @@
 require_relative 'toy_robot'
 require 'pry-byebug'
 
+def welcome_message
+  puts '--- TOY ROBOT GAME ---'
+  puts 'Input your first command (first command must be PLACE X, Y, DIRECTION)'
+end
+
+def first_input_message
+  puts 'First command must be PLACE X, Y, DIRECTION'
+end
+
+def wrong_input_message
+  puts 'Wrong input, try again.'
+end
+
+def goodbye_message
+  puts 'Goodbye - thanks for coming by.'
+end
+
 def run
   robot = ToyRobot.new
 
-  puts '--- TOY ROBOT GAME ---'
-  puts 'Input your first command (first command must be PLACE X, Y, DIRECTION)'
+  welcome_message
 
   input = gets.chomp.upcase
   until input.start_with?('PLACE')
-    puts 'First command must be PLACE X, Y, DIRECTION'
+  # until input.start_with?('PLACE')
+    first_input_message
     input = gets.chomp.upcase
   end
 
@@ -28,10 +45,10 @@ def run
     when 'REPORT'
       puts robot.report
     when 'END'
-      puts 'Goodbye'
+      goodbye_message
       break
     else
-      puts 'Wrong input, try again.'
+      wrong_input_message
     end
     input = gets.chomp.upcase
   end
